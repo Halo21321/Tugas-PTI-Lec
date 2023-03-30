f46496a2736f017137ade63f7725db9f71fa0e77
Halo21321 <129380583+Halo21321@users.noreply.github.com>
2023-03-30 16:26:44 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,38 @@
+const name1=document.getElementById('name')
+const form=document.getElementById('form1')
+
+form.addEventListener('submit', e=>
+{
+    let messages=[]
+    if (name1.value==''||name1.value==NULL){//Jika nama tidak dimasukkan
+        messages.push('Name is required')//Memunculkan sebuah pesan
+    }
+    e.preventDefault()
+})
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
