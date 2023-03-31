45a5c369d80803d78af1053f87a7859c93300ece
Halo21321 <129380583+Halo21321@users.noreply.github.com>
2023-03-31 10:55:33 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -36,3 +36,42 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 } 
+
+var healthLevel = 100;
+var healthBar = document.getElementById("health-level");
+
+document.getElementById("health-button").addEventListener("click", function() {
+  healthLevel += 10;
+  healthBar.style.width = healthLevel + "%";
+});
+
+setInterval(function() {
+  healthLevel -= 5;
+  if (healthLevel < 0) {
+    healthLevel = 0;
+  }
+  healthBar.style.width = healthLevel + "%";
+}, 10);
+
+function displayTime(){
+  var dateTime = new Date();
+  var hrs = dateTime.getHours();
+  var min = dateTime.getMinutes();
+  var sec = dateTime.getSeconds();
+  var session = document.getElementById('session');
+
+  if(hrs >= 12){
+      session.innerHTML = 'PM';
+  }else{
+      session.innerHTML = 'AM';
+  }
+
+  if(hrs > 12){
+      hrs = hrs - 12;
+  }
+
+  document.getElementById('hours').innerHTML = hrs;
+  document.getElementById('minutes').innerHTML = min;
+  document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
